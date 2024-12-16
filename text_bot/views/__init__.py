f368eaebc8e0ac48cd12568feeb7b23b4d919238
Milan Misic <milandm@gmail.com>
2024-12-16 11:14:25 +0100

    sales agent view removed

--- text_bot/views/__init__.py
+++ text_bot/views/__init__.py
@@ -1,6 +1,5 @@
 from .text_bot_views import PublicTextExtractionAPIView
 from .text_bot_views import PublicTextBotAPIView
-from .text_bot_views import PublicSlesAgentAPIView
 from .text_bot_views import TextBotAPIView
 from .text_bot_views import UserHistoryListView
 from .text_bot_views import DeleteUserHistoryView
